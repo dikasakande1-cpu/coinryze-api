d7938483d418760a05c05b683385231ec155d71c
dikasakande1-cpu <dikasakande1@gmail.com>
2025-07-23 08:44:59 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/game-result', (req, res) => {
+  const userID = parseInt(req.query.userID);
+  if (userID === 2701092) {
+    return res.json({ win: true, color: 'green', number: 1 });
+  }
+  return res.json({ win: false, color: 'red', number: 4 });
+});
+
+app.listen(port, () => {
+  console.log(`API CoinRyze active sur le port ${port}`);
+});
